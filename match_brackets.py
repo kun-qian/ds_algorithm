f6751b33e178de4e366069e6000506054005a218
kun.qian <kun.qian@orange.com>
2017-05-24 10:56:34 +0800

    bracket match

--- match_brackets.py
+++ match_brackets.py
@@ -1,2 +1,31 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+__author__ = 'kun qian'
+
+
+# if the string only contains '(', ')'
+# tell if the text match ()
+def match_bracket(text):
+    stack = []
+    for v in text:
+        if v == '(':
+            stack.append(v)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(match_bracket('((())()())'))
+
+
+def match_all_brackets(text):
+    s = []
+    for v in text:
+        if v in '([{':
+            s.append(v)
+        else:
+            if len(s) == 0:
+                return False
