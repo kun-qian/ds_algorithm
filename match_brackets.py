8f59ed51087c0ffd5f188153fb4e7c1f65784c65
kun.qian <kun.qian@orange.com>
2017-05-24 11:08:46 +0800

    bracket match

--- match_brackets.py
+++ match_brackets.py
@@ -1,31 +1,45 @@
 #!usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'kun qian'
+__author__ = 'Kun'
 
 
-# if the string only contains '(', ')'
-# tell if the text match ()
-def match_bracket(text):
-    stack = []
-    for v in text:
-        if v == '(':
-            stack.append(v)
-        else:
-            if len(stack) == 0:
-                return False
-            else:
-                stack.pop()
-    return len(stack) == 0
-
-print(match_bracket('((())()())'))
-
-
-def match_all_brackets(text):
+# strings only contain '(', ')'
+def simple_checker(strings):
     s = []
-    for v in text:
+
+    for v in strings:
+        if v == '(':
+            s.append(v)
+        else:
+            if len(s) != 0:
+                s.pop()
+            else:
+                return False
+
+    return len(s) == 0
+
+
+# strings contain ()[]{}
+def checker(strings):
+    s = []
+    for v in strings:
         if v in '([{':
             s.append(v)
         else:
             if len(s) == 0:
                 return False
+            else:
+                if not match(s.pop(), v):
+                    return False
+    return len(s) == 0
+
+
+def match(open_tag, close_tag):
+    opens = '{[('
+    closes = '}])'
+    return opens.index(open_tag) == closes.index(close_tag)
+
+# print(simple_checker("(())()())"))
+# print(simple_checker('((()))'))
+# print(checker('{[()]}'))
