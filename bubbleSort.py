e03347a34cf439b5caa146250f9484af7938b32c
kun.qian <kun.qian06@gmail.com>
2017-10-16 13:17:51 +0800

    bubble sort

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(alist):
+    for i in range(len(alist) - 1, 0, -1):
+        for j in range(i):
+            if alist[j] > alist[j + 1]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+    return alist
+
+
+# print(bubbleSort([1, 52, 4, 6, 66, 7]))
