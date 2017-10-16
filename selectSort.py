1c724c24395a93bd57ff0c371fc618f9fc40e360
kun.qian <kun.qian06@gmail.com>
2017-10-16 16:03:05 +0800

    select sort descending

--- selectSort.py
+++ selectSort.py
@@ -1,4 +1,4 @@
-def selectSort(alist):
+def selectSortAsc(alist):
     for i in range(len(alist) - 1, 0, -1):
         maxpos = 0
         for j in range(1, i + 1):
@@ -7,5 +7,14 @@ def selectSort(alist):
         alist[i], alist[maxpos] = alist[maxpos], alist[i]
     return alist
 
+def selectSortDesc(alist):
+    for i in range(len(alist)):
+        maxpos = i
+        for j in range(i + 1, len(alist)):
+            if alist[j] > alist[maxpos]:
+                maxpos = j
+        alist[i], alist[maxpos] = alist[maxpos], alist[i]
+    return alist
 
-print(selectSort([1, 152, 4, 6, 66, 7, 3]))
+print(selectSortAsc([1, 152, 4, 6, 66, 7, 3]))
+print(selectSortDesc([1, 152, 4, 6, 66, 7, 3]))
