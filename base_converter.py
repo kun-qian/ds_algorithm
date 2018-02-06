bdc8a34636c1af25739e77b54584cf8f9bd84207
kun.qian <kun.qian06@gmail.com>
2018-02-06 16:05:38 +0800

    recursive

--- base_converter.py
+++ base_converter.py
@@ -10,5 +10,16 @@ def base_converter(n, base):
         res += label[stack.pop()]
     return res
 
+
+def base_converter_re(n, base):
+    base_string = '0123456789ABCDEF'
+    if n < base:
+        return base_string[n]
+    else:
+        return base_converter(n // base, base) + base_string[n % base]
+
+
 print(base_converter(32, 2))
-print(base_converter(32, 8))
+print(base_converter(3699, 8))
+print(base_converter_re(32, 2))
+print(base_converter_re(3699, 8))
