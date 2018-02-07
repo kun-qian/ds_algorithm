c63b8370a9a907403dee5b2703ad81f4bb99c4b8
kun.qian <kun.qian06@gmail.com>
2018-02-07 17:08:08 +0800

    bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,9 +1,19 @@
-def bubbleSort(alist):
-    for i in range(len(alist) - 1, 0, -1):
-        for j in range(i):
+def bubble_sort_asc(alist):
+    for i in range(len(alist) - 1):
+        for j in range(len(alist) - i - 1):
             if alist[j] > alist[j + 1]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+
+    return alist
+
+
+def bubble_sort_des(alist):
+    for i in range(len(alist) - 1):
+        for j in range(len(alist) - 1 - i):
+            if alist[j] < alist[j + 1]:
                 alist[j], alist[j + 1] = alist[j + 1], alist[j]
     return alist
 
 
-# print(bubbleSort([1, 52, 4, 6, 66, 7]))
+print(bubble_sort_asc([3, 43, 1, 45, 67, 34]))
+print(bubble_sort_des([3, 43, 1, 45, 67, 34]))
