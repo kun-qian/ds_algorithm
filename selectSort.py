f2a074a3c5fb6c65c38fa1a88966f71ec92f4ce0
kun.qian <kun.qian06@gmail.com>
2018-02-08 16:15:57 +0800

    select sort

--- selectSort.py
+++ selectSort.py
@@ -1,22 +1,27 @@
 def select_sort_asc(alist):
-    for i in range(len(alist) - 1, 0, -1):
+    length = len(alist)
+    for i in range(length - 1):
         maxpos = 0
-        for j in range(1, i + 1):
+        for j in range(1, length - i):
             if alist[j] > alist[maxpos]:
                 maxpos = j
-        alist[i], alist[maxpos] = alist[maxpos], alist[i]
+
+        alist[length - 1 - i], alist[maxpos] = alist[maxpos], alist[length - 1 -
+                                                                    i]
     return alist
 
 
-def select_sort_desc(alist):
-    for i in range(len(alist)):
+def select_sort_des(alist):
+    length = len(alist)
+    for i in range(length - 1):
         maxpos = i
-        for j in range(i + 1, len(alist)):
+        for j in range(i + 1, length):
             if alist[j] > alist[maxpos]:
                 maxpos = j
         alist[i], alist[maxpos] = alist[maxpos], alist[i]
+
     return alist
 
 
-print(select_sort_asc([1, 152, 4, 6, 66, 7, 3]))
-print(select_sort_desc([1, 152, 4, 6, 66, 7, 3]))
+print(select_sort_asc([1, 4, 56, 43, 5, 66]))
+print(select_sort_des([1, 4, 56, 43, 5, 66]))
