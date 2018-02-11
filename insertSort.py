7e8a52d30dae57ed15bd6673911f18f6d588ab67
kun.qian <kun.qian06@gmail.com>
2018-02-11 11:33:15 +0800

    insert sort

--- insertSort.py
+++ insertSort.py
@@ -1,12 +1,24 @@
-def insertSort(alist):
+def insertion_sort_asc(alist):
     for i in range(1, len(alist)):
-        currentValue = alist[i]
+        current = alist[i]
         position = i
-        while position > 1 and alist[position - 1] > currentValue:
+        while position > 0 and alist[position - 1] > current:
             alist[position] = alist[position - 1]
             position -= 1
-        alist[position] = currentValue
+        alist[position] = current
     return alist
 
 
-print(insertSort([1, 152, 4, 6, 66, 7]))
+def insertion_sort_des(alist):
+    for i in range(1, len(alist)):
+        current = alist[i]
+        position = i
+        while position > 0 and alist[position - 1] < current:
+            alist[position] = alist[position - 1]
+            position -= 1
+        alist[position] = current
+    return alist
+
+
+print(insertion_sort_asc([1, 152, 4, 6, 66, 7]))
+print(insertion_sort_des([1, 152, 4, 6, 66, 7]))
